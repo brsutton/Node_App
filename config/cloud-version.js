5c9b46cd4a635421127488d602354a5b9a093c2d
Brian Sutton <brsutton@icloud.com>
2017-12-22 15:39:40 -0600

    Added a cloud-version.js file

---
+++ config/cloud-version.js
@@ -0,0 +1,6 @@
+var cloudVersion = {};
+
+cloudVersion.region = 'us-east-1';
+cloudVersion.apiVersion = '2012-08-10';
+
+module.exports = cloudVersion;
--- src/getUsersFromDatabase.js
+++ src/getUsersFromDatabase.js
@@ -1,13 +1,14 @@
 var AWS = require('aws-sdk');
+var cloudVersion = require('../config/cloud-version.js');
 
 exports.getUserFromDatabase = function (username) {
 
-    AWS.config.update({ region: 'us-east-1' });
-    var docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+    AWS.config.update({ region: cloudVersion.region });
+    var docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: cloudVersion.apiVersion });
     var params = {
         TableName: 'node_web_app_users',
         Key: { 'username': username }
     };
-    
+
     return docClient.get(params).promise();
 };
