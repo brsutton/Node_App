76dd4831ac9d0ce837d0079a4eb5b25bc01866b5
Brian Sutton <brsutton@icloud.com>
2017-12-22 12:49:12 -0600

    Make use of sinon in validation testing

--- test/getUsersFromDatabase.js
+++ test/getUsersFromDatabase.js
@@ -1,19 +1,54 @@
-var request = require("request");
-var assert = require('assert');
-const getUserFromDatabase = require('../src/getUsersFromDatabase.js');
 
-describe('Get Users From Database', async ()=> {
-    it('get user brian', function () {
-        const user = getUserFromDatabase.getUserFromDatabase('brian')
-        .then(function(data){
-           // console.log(data);
-            assert.equal('brian', data.Item.username );
+const assert = require('assert');
+const sinon = require('sinon');
+const getUserFromDatabase = require('../src/getUsersFromDatabase.js');
+var sandbox;
+
+beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+});
+
+afterEach(function () {
+    sandbox.restore();
+});
+
+describe('Get Users From Database Success', () => {
+    it('get user brian', function (done) {
+        var stub = sandbox.stub(getUserFromDatabase, 'getUserFromDatabase');
+        stub.resolves({
+            Item:
+                {
+                    password: '$2a$10$3wk6zPkL9UrTOZuoEgow7.9Q.lpKSBnPyp01uu.WNSpi9LKb.iFkm',
+                    username: 'brian',
+                    lastName: 'Sutton',
+                    firstName: 'Brian'
+                }
+        });
+
+        const data = getUserFromDatabase.getUserFromDatabase('brian');
+
+        data.then(function (data) {
+            assert.equal('brian', data.Item.username);
             assert.equal('Brian', data.Item.firstName);
             assert.equal('Sutton', data.Item.lastName);
+            done();
         })
-        .catch(function(err){
-            console.log(err);
+    });
+});
+
+describe('Get Users From Database Error', () => {
+    it('error from database', function (done) {
+
+        var stub = sandbox.stub(getUserFromDatabase, 'getUserFromDatabase');
+        stub.rejects('TypeError');
+        const data = getUserFromDatabase.getUserFromDatabase('notUser');
+
+        data.then(function () {
+
         })
-        
+            .catch(function (err) {
+                assert.equal('TypeError', err);
+                done();
+            });
     });
 });
--- test/validate.js
+++ test/validate.js
@@ -1,19 +1,75 @@
 var request = require("request");
 var validate = require("../src/validate.js");
+const getUserFromDatabase = require('../src/getUsersFromDatabase.js');
 var assert = require('assert');
+const sinon = require('sinon');
+var sandbox;
 
-describe('Validate', async ()=> {
-    it('validate user brian', function () {
+process.on('unhandledRejection', function (err) {
+    throw err;
+});
+
+beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+});
+
+afterEach(function () {
+    sandbox.restore();
+});
+
+describe('Validate', () => {
+    it('validate user brian', function (done) {
+        var stub = sandbox.stub(getUserFromDatabase, 'getUserFromDatabase');
+        stub.resolves({
+            Item:
+                {
+                    password: '$2a$10$3wk6zPkL9UrTOZuoEgow7.9Q.lpKSBnPyp01uu.WNSpi9LKb.iFkm',
+                    username: 'brian',
+                    lastName: 'Sutton',
+                    firstName: 'Brian'
+                }
+        });
         validate.validate(request, 'brian', 'brian', 'h')
-        .then(function(data){
-            assert.equal(true, data.isValid );
-            assert.equal('brian', data.credentials.username );
-            assert.equal('Brian', data.credentials.firstName );
-            assert.equal('Sutton', data.credentials.lastName );
-        })
-        .catch(function(err){
-            console.log(err)
-        })
-        
+            .then(function (data) {
+                assert.equal(true, data.isValid);
+                assert.equal('brian', data.credentials.username);
+                assert.equal('Brian', data.credentials.firstName);
+                assert.equal('Sutton', data.credentials.lastName);
+                done();
+            })
+    });
+});
+
+describe('Not Validated', () => {
+    it('not validated user bran', function (done) {
+        var stub = sandbox.stub(getUserFromDatabase, 'getUserFromDatabase');
+        stub.resolves({});
+        validate.validate(request, 'bran', 'bran', 'h')
+            .then(function (data) {
+                assert.equal(false, data.isValid);
+                assert.equal(null, data.credentials);
+                done();
+            })
+    });
+
+    it('not valid password user brian', function (done) {
+        var stub = sandbox.stub(getUserFromDatabase, 'getUserFromDatabase');
+        stub.resolves({
+            Item:
+                {
+                    password: '$2a$10$3wk6zPkL9UrTOZuoEgow7.9Q.lpKSBnPyp01uu.WNSpi9LKb.iFkm',
+                    username: 'brian',
+                    lastName: 'Sutton',
+                    firstName: 'Brian'
+                }
+        });
+        validate.validate(request, 'brian', 'invalidPassword', 'h')
+            .then(function (data) {
+                assert.equal(false, data.isValid);
+                assert.equal('brian', data.credentials.username);
+                assert.equal('Brian', data.credentials.firstName);
+                assert.equal('Sutton', data.credentials.lastName);
+                done();
+            })
     });
 });
